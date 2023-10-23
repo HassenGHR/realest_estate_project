284e6e39e4cb90c002e4bc6b1af36c5c0e41c962
HassenGHR <109977314+HassenGHR@users.noreply.github.com>
2023-10-23 14:18:14 +0100

    adjust settings file

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -77,19 +77,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'real_estate.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'hassan120860',
-        'HOST': 'localhost',
-        'PORT':'5433'
-    }
-}
 
 
 # Password validation
@@ -131,12 +118,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "karim.sabe2383@gmail.com"
-EMAIL_HOST_PASSWORD = "trpeioawlomnpzqn"
+
 
 
 # Static files (CSS, JavaScript, Images)
